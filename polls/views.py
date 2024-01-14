76b00f81efcbf3fdad5e0651e5a0eeafe9757f9f
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-14 17:43:06 -0500

    sace

--- polls/views.py
+++ polls/views.py
@@ -5,22 +5,23 @@ from django.urls import reverse
 from .models import Choice, Question
 
 
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    context = {
-        'latest_question_list': lastest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/detail.html', {'question':question})
 
-def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
