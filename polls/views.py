5f2121cd2e147f56b79a875302c5ca8b0cf187f5
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-14 17:19:18 -0500

    sace

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -14,8 +15,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
-
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question':question})
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
 
